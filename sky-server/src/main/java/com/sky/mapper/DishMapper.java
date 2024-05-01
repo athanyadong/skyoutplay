16ffc0f5f11c45b530924c67648c62e0906d7937
athan <hanyadong1010@gmail.com>
2024-05-01 17:24:29 +0800

    菜品删除功能完成

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annoutation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -28,4 +32,21 @@ public interface DishMapper {
     @Select("select count(name) from dish where name=#{name}")
     Integer selectByDishName(String name);
 
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据主键查询菜品
+     * @param id
+     * @return
+     */
+    @Select("select *from dish where id = #{id}")
+    Dish getById(Long id);
+
+    @Delete("delete from dish where id= #{id}")
+    void deleteById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,11 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface DishService {
 
@@ -10,4 +14,12 @@ public interface DishService {
      * @param dishDTO
      */
     void saveWith(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 批量删除功能
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
